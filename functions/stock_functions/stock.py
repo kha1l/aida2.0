ed88bbbbd137708eee661942955bc63199ca9c10
dev-vkus <dev-work8@outlook.com>
2023-10-12 00:15:34 +0300

    pre-release/ex-fix3.5

--- functions/stock_functions/stock.py
+++ functions/stock_functions/stock.py
@@ -1,7 +1,6 @@
 from database.postgres_async import AsyncDatabase
 from utils.sending import Send
 from loggs.logger import Log
-from datetime import datetime
 
 
 async def send_stock():
@@ -10,11 +9,8 @@ async def send_stock():
     send = Send(db=db)
     logger = Log('stock')
     orders = await db.select_orders(pool, 'stock')
-    dt = datetime.now().date()
-    dt_day = datetime.strftime(dt, '%d.%m.%Y')
     for order in orders:
         items = await db.select_items(pool, order['uuid'])
-        message = f'\U0001F9C0 Складские остатки на {dt_day}:\n\n'
         unit_prev = ''
         for item in items:
             value = item['quantity']
@@ -22,11 +18,12 @@ async def send_stock():
             item_name = item['name']
             unit = item['unit']
             meas = item['measurement']
+            message = f'\U0001F9C0 Складские остатки в {unit}:\n\n'
             if avg_value:
-                if value <= avg_value:
+                if value * 2 <= avg_value:
                     if unit != unit_prev:
                         message += f'<b>{unit}</b>\n'
-                    message += f'<b>{item_name}</b> заканчивается запас!! \U000026A0\n'
+                    message += f'<b>{item_name}</b> заканчивается запас \U000026A0\n'
                     unit_prev = unit
                     if meas == 'шт':
                         value = int(value)
@@ -36,6 +33,6 @@ async def send_stock():
                     if value < 0:
                         message += f'(проверьте внесен ли приход или корректные ли замеры)\n'
                     message += f'на 2 дня необходимо {avg_value} {meas}\n\n'
-        if message != f'\U0001F9C0 Складские остатки на {dt_day}:\n\n':
-            await send.sending(order["chat_id"], message, logger, order['id'])
+            if message != f'\U0001F9C0 Складские остатки в {unit}:\n\n':
+                await send.sending(order["chat_id"], message, logger, order['id'])
     await pool.close()
